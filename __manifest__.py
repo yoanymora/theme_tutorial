b96fd4b86f79d4c7f846957ccebaa5488b3756e0
yoany mora <yoany@vauxoo.com>
2020-12-01 17:36:25 -0600

    [ADD] Add base code for theme_tutorial module; Add services page and edit website main layout to practice front end developement in Odoo 12.0

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Theme Tutorial',
+    'description': 'A theme tutorial app',
+    'version': '13.0',
+    'author': 'Vauxoo',
+    'data': [
+        'views/assets.xml',
+        'views/pages.xml',
+        'views/layout.xml',
+    ],
+    'category': 'Theme/Creative', # this line is mandatory, add this theme to a list of available themes to install
+    'depends': [
+        'website_theme_install',
+    ],
+}
