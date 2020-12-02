1a54a91337453917ab75bf67ab3ec0d70f491d1f
yoany mora <yoany@vauxoo.com>
2020-12-02 00:09:23 -0600

    [ADD] Add snippet: an element which you can drag and drop into a div.oe_structure; this have a problem: the js and option code are not working as they should

--- __manifest__.py
+++ __manifest__.py
@@ -5,8 +5,11 @@
     'author': 'Vauxoo',
     'data': [
         'views/assets.xml',
+        'views/image_gallery.xml',
         'views/pages.xml',
         'views/layout.xml',
+        'views/snippets.xml',
+        'views/options.xml',
     ],
     'category': 'Theme/Creative', # this line is mandatory, add this theme to a list of available themes to install
     'depends': [
---
+++ static/js/tutorial_editor.js
@@ -0,0 +1,10 @@
+odoo.define('theme_tutorial.snippet_testimonial_options', function (require) {
+    'use strict';
+
+    var options = require('web_editor.snippets.options');
+    options.registry.snippet_testimonial_options = options.Class.extend({
+        onFocus: function(){
+            alert("On Focus!");
+        }
+    });
+});
